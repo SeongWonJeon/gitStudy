ca01e83f920906ed1ed3e47b97404e97f8c0f9fa
윤진 소 <pms010506@gmail.com>
2023-04-17 18:48:43 +0900

    Feat: 순대의 맛있음을 어필

--- gitStudy/yoonjin workspace.cs
+++ gitStudy/yoonjin workspace.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Channels;
 
 namespace gitStudy
 {
     internal class yoonjin_workspace
     {
+        static void yoonjin()
+        {
+            Console.WriteLine("순대는 맛있어요.");
+        }
+       
     }
 }
